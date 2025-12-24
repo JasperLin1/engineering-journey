e7092b98d1440dc28fb28a8a2cbe444c0e72c0c3
JasperLin1 <o911777040@gmail.com>
2025-12-24 14:15:17 +0800

    practice asynchronous design exercise

---
+++ day8/Practical asynchronous design.js
@@ -0,0 +1,28 @@
+// ex1
+async function foo() {
+  console.log("1");
+  await bar();
+  console.log("3");
+}
+
+async function bar() {
+  console.log("2");
+}
+
+foo();
+
+// ex2(compare with ex1)
+
+async function foo() {
+  console.log("1");
+  await Promise.resolve();
+  console.log("3");
+}
+
+async function bar() {
+  console.log("2");
+}
+
+foo();
+bar();
+
